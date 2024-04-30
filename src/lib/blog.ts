329504f6efd84127c0b4e5bac20e11685ed12e86
tozydev <tozydev@nguyenthanhtan.id.vn>
2024-04-30 15:42:29 +0700

    fix: toSorted method not found

--- src/lib/blog.ts
+++ src/lib/blog.ts
@@ -15,7 +15,7 @@ function compareByDate(a: CollectionEntry<"blog">, b: CollectionEntry<"blog">, s
 
 export async function getBlogPosts({ sortBy, limit }: Partial<Options> = {}) {
   return getCollection<"blog">("blog", (post) => !post.data.draft).then((posts) => {
-    const sorted = posts.toSorted((a, b) => compareByDate(a, b, sortBy || "mod-date"))
+    const sorted = posts.sort((a, b) => compareByDate(a, b, sortBy || "mod-date"))
     return (limit || 0) > 0 ? sorted.slice(0, limit) : sorted
   })
 }
